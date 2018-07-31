3d12aade1169e99735022fb922289c702ac3f312
Aurélien  DIGOUT <aurelien.digout@gmail.com>
2018-07-30 21:15:12 -0400

    change for dynamic remotes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
-  var routes = require('./api/routes/ac.routes'); //importing route
+  var routes = require('./api/routes/ir.routes'); //importing route
   routes(app); //register the route
 
   app.listen(port);
